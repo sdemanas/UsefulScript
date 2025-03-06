8ed9a05d2411b43e3d1fe91249a1f1ce86509ba9
Manas <194718544+sdemanas@users.noreply.github.com>
2025-03-06 11:40:04 +0530

    Feat : Poweshell - Find Strings

---
+++ powershell/FindStrings.ps1
@@ -0,0 +1,16 @@
+# FindStrings is a powershell script to find a unique string
+
+# Searches for the InputString recursively in files stored in current parent & sub directories 
+
+# String Input
+$InputString = Read-Host 'Input the string to search'
+
+# Recursively list all files recursively where "InputString" is found 
+Get-ChildItem -Path . -Recurse -File | 
+
+# Use regex to strictly search for InputString 
+# Select-String -Pattern "\b$name\b" 
+Select-String -Pattern $InputString |
+
+# keep only unique file paths in the result.
+Select-Object -Unique Path
